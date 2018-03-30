3a56a3875df7ba994b9bf9e9ece633a121b34ac2
The_Fireplace <the.f1repl4ce@gmail.com>
2018-03-30 17:51:08 -0500

    Improvement

--- src/main/java/the_fireplace/rcad/RainingCatsAndDogs.java
+++ src/main/java/the_fireplace/rcad/RainingCatsAndDogs.java
@@ -7,24 +7,23 @@ import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-@Mod(modid=RainingCatsAndDogs.MODID, name=RainingCatsAndDogs.MODNAME, acceptableRemoteVersions = "*", guiFactory = "the_fireplace.rcad.client.RCADConfigGui")
+@Mod(modid = RainingCatsAndDogs.MODID, name = RainingCatsAndDogs.MODNAME, version = "${version}", acceptableRemoteVersions = "*", guiFactory = "the_fireplace.rcad.client.RCADConfigGui")
 @Mod.EventBusSubscriber
 public class RainingCatsAndDogs {
 	public static final String MODNAME = "Raining Cats and Dogs";
 	public static final String MODID = "rcad";
 
 	@SubscribeEvent
-	public static void onPlayerTick(TickEvent.PlayerTickEvent event){
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
 		World world = event.player.world;
-		if(!world.isRemote){
+		if (!world.isRemote) {
 			long time = world.getTotalWorldTime();
-			if(time % (ConfigValues.time_between_animals * 20) == 0 && world.provider.getDimensionType() == DimensionType.OVERWORLD && world.isThundering()){
-				BlockPos spawnPos = new BlockPos(event.player.getPosition().getX()+world.rand.nextInt(ConfigValues.animal_spawn_radius*2)-ConfigValues.animal_spawn_radius, ConfigValues.animal_spawn_height, event.player.getPosition().getZ()+world.rand.nextInt(ConfigValues.animal_spawn_radius*2)-ConfigValues.animal_spawn_radius);
-				if(world.rand.nextBoolean()){
+			if (time % (ConfigValues.time_between_animals * 20) == 0 && world.provider.getDimensionType() == DimensionType.OVERWORLD && world.isThundering()) {
+				BlockPos spawnPos = new BlockPos(event.player.getPosition().getX() + world.rand.nextInt(ConfigValues.animal_spawn_radius * 2) - ConfigValues.animal_spawn_radius, ConfigValues.animal_spawn_height, event.player.getPosition().getZ() + world.rand.nextInt(ConfigValues.animal_spawn_radius * 2) - ConfigValues.animal_spawn_radius);
+				if (world.rand.nextBoolean()) {
 					EntityOcelot newCat = new EntityOcelot(world);
 					newCat.setPosition(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
 					world.spawnEntity(newCat);
@@ -34,7 +33,7 @@ public class RainingCatsAndDogs {
 					newWolf.setPosition(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
 					world.spawnEntity(newWolf);
 					newWolf.setPositionAndRotation(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), world.rand.nextFloat(), world.rand.nextFloat());
-					if(ConfigValues.angry_wolves && world.rand.nextInt(10) != 5)
+					if (ConfigValues.angry_wolves && world.rand.nextInt(10) != 5)
 						newWolf.setAngry(true);
 				}
 			}
@@ -42,10 +41,10 @@ public class RainingCatsAndDogs {
 	}
 
 	@Config(modid = MODID)
-	public static class ConfigValues{
+	public static class ConfigValues {
 		@Config.Comment("Time between animals (In seconds)")
 		@Config.LangKey("time_between_animals")
-		@Config.RangeInt(min=1)
+		@Config.RangeInt(min = 1)
 		public static int time_between_animals = 5;
 
 		@Config.Comment("Can wolves be angry about falling from the sky?")
@@ -54,12 +53,12 @@ public class RainingCatsAndDogs {
 
 		@Config.Comment("Animal Spawn Radius around the player")
 		@Config.LangKey("animal_spawn_radius")
-		@Config.RangeInt(min=1)
+		@Config.RangeInt(min = 1)
 		public static int animal_spawn_radius = 64;
 
 		@Config.Comment("Animal Spawn Height")
 		@Config.LangKey("animal_spawn_height")
-		@Config.RangeInt(min=2)
+		@Config.RangeInt(min = 2)
 		public static int animal_spawn_height = 255;
 	}
 }
